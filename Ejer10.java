6e22578da07dbecc2b310e33db34a249cbcd941b
V-Rosales03 <79428500+V-Rosales03@users.noreply.github.com>
2025-07-14 23:56:05 -0300

    Update and rename Ejercicio10.java to Ejer10.java

--- Ejer10.java
+++ Ejer10.java
@@ -1,5 +1,4 @@
-
-public class Ejercicio10 {
+public class Ejer10 {
     public static void main(String[] args) {
         int a = 2, b = 7, c = 5;
         int mayor = (a >= b && a >= c) ? a : (b >= c ? b : c);
